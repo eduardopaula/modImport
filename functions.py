68f6107c3a93ce434394ca90f31434a152f76be5
Eduardo Neves <eduardo.paula@gmail.com>
2024-03-06 22:30:19 -0300

    Inicio da Aula 320

--- functions.py
+++ functions.py
@@ -5,21 +5,19 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def centre_text(*args, sep=' ', end_char='\n', centered_file=None, flush_me=False):
+def centre_text(*args, sep=' ', end='\n', file=None, flush=False):
     text = ""
     for arg in args:
         text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text, end=end_char, file=centered_file, flush=flush_me)
+    print(" " * left_margin, text, end=end, file=file, flush=flush)
 
 
-# call the function
-# python_food()
+with open("centred", mode='w') as centred_file:
+    centre_text("spam and eggs", file=centred_file)
+    centre_text("spam, spam and eggs", file=centred_file)
+    centre_text("spam, spam, spam and eggs", file=centred_file)
+    centre_text('12', file=centred_file)
+    centre_text("spam, spam, spam and spam", file=centred_file)
 
-centre_text("spam and eggs")
-centre_text("spam, spam and eggs")
-centre_text("spam, spam, spam and eggs")
-centre_text('12')
-centre_text("spam, spam, spam and spam")
-
-centre_text("first", "second", 3, 4, "spam", sep=":")
+    centre_text("first", "second", 3, 4, "spam", sep=":", file=centred_file)
