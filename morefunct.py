4a759a026f57dcb11f91a3fe1fbf75bf75bec534
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-03-07 09:00:51 -0300

    Aula 323

--- morefunct.py
+++ morefunct.py
@@ -16,6 +16,7 @@ def draw_axes(canvas):
     canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
     canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
     canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+    print(locals())
 
 
 def plot(canvas, x, y):
@@ -26,10 +27,15 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
 
-canvas = tkinter.Canvas(mainWindow, width=640, height=480)
+canvas = tkinter.Canvas(mainWindow, width=320, height=480)
 canvas.grid(row=0, column=0)
 
+canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background="blue")
+canvas2.grid(row=0, column=1)
+
+print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
+draw_axes(canvas2)
 
 for x in range(-100, 100):
     y = parablola(x)
